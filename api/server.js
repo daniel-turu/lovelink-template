889ee9fb6b42ad84dbc4f1e652e197771c57f1bc
daniel-turu <danielturu30@gmail.com>
2024-08-02 03:09:13 +0100

    setup for vercel deploy

--- api/server.js
+++ api/server.js
@@ -34,13 +34,16 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.static(path.join('build')));
+// Serve static files from the "build" directory
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join('build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 const port = process.env.PORT || 3003;
-app.listen(port, () => { console.log(`Server running on port ${port}`)});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 module.exports = app;
