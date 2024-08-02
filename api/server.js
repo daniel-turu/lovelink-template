8a039c261e836bcd5c3208043162e437b9b18db8
daniel-turu <danielturu30@gmail.com>
2024-08-02 02:51:14 +0100

    setup for vercel deploy

--- api/server.js
+++ api/server.js
@@ -34,10 +34,10 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join('build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join('build', 'index.html'));
 });
 
 const port = process.env.PORT || 3003;
