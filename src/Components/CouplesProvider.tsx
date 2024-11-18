8e4db41e102356c524ff8fe3c3e2c75c6c3e861d
daniel-turu <danielturu30@gmail.com>
2024-11-18 12:32:11 +0100

    added loading while waiting for data to load

--- src/Components/CouplesProvider.tsx
+++ src/Components/CouplesProvider.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, ReactNode, useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import {  Couplesdata, getCouples, getEmptyCouples } from '../CouplesData';
+import { Couplesdata, getCouples, getEmptyCouples } from '../CouplesData';
 import { getCouplesByReference } from '../Utiles';
 
 // Create the context
@@ -22,13 +22,16 @@ export function CouplesProvider({ children }: { children: ReactNode }) {
   const reference = queryParams.get('reference'); // Extract referenceId from query parameters
 
   const [couplesData, setCouplesData] = useState<Couplesdata>(getEmptyCouples); // Initialize as empty couples data
+  const [loading, setLoading] = useState(true); // New loading state
 
   useEffect(() => {
     const fetchCouplesData = async () => {
+      setLoading(true); // Set loading to true before fetching data
       if (reference) {
         try {
           const response = await getCouplesByReference(reference);
           const data = response as Couplesdata;
+          
           if (data) {
             setCouplesData(data);
           }
@@ -38,10 +41,16 @@ export function CouplesProvider({ children }: { children: ReactNode }) {
       } else {
         setCouplesData(getCouples()); // Set default couples data if no referenceId
       }
+      setLoading(false); // Set loading to false after fetching data
     };
   
     fetchCouplesData();
   }, [reference]);
+
+  // Show loader if loading
+  if (loading) {
+    return <div>Loading...</div>; // Replace with your loader component or animation
+  }
 
   return (
     <CouplesContext.Provider value={couplesData}>
@@ -49,4 +58,3 @@ export function CouplesProvider({ children }: { children: ReactNode }) {
     </CouplesContext.Provider>
   );
 }
-
